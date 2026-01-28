f6f772cf651b70470994875073aa379a60007f1d
SHASHANK SINGH <shashank@SHASHANKs-MacBook-Air.local>
2026-01-29 01:05:53 +0530

    deployment ready

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,15 @@ import dotenv from "dotenv";
 dotenv.config();
 import app from "./src/app.js";
 import  connectDB  from "./src/config/db.js";
+import path from "path";
+import express from "express";
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/dist/index.html'));
+});
+
 
 const start = async () => {
   try {
